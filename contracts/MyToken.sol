ec61fbd96407ef189b798584989a53b6b151d5ff
harshita808 <harshitasaxenah7@gmail.com>
2022-07-15 18:02:02 +0530

    ERC20 contract

---
+++ contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+       // _mint(msg.sender, 100 * 10**uint(decimals()));
+    }
+
+function mint(address account, uint256 amount) public{
+     _mint(msg.sender, 100 * 10**uint(decimals()));
+}    
+}
--- test/TokenSwapTest.js
+++ test/TokenSwapTest.js
@@ -1,6 +1,7 @@
 const { expectRevert } = require('@openzeppelin/test-helpers');
-const Token = artifacts.require("TokenSwap.sol");
-const erc20 = artifacts.require("IERC20.sol");
+const TokenSwap = artifacts.require("TokenSwap.sol");
+const ERC20Token = artifacts.require("MyToken.sol");
+//const erc20 = artifacts.require("IERC20.sol");
 
 contract("TokenSwap", (accounts) => { let token; 
     const amount1 = web3.utils.toBN(web3.utils.toWei("1"));
@@ -8,48 +9,49 @@ contract("TokenSwap", (accounts) => { let token;
      
 beforeEach(async () => { 
     // let contract_addr= await Token.deployed();
-    token = await Token.deployed(); 
+    tokenSwap = await TokenSwap.deployed(); 
+    erc20Token = await ERC20Token.deployed();
 });
 
 it("should not allow swapping if called by non-owner", async () => {
-    await expectRevert(token.swap(),"Not authorized");
+    await expectRevert(tokenSwap.swap(),"Not authorized");
 });
 
 it("should swap token1 when approved", async () => { let allowance; let receipt; const value = web3.utils.toBN(100);
 
-    allowance = await token.allowance(accounts[1], token.address);
+    allowance = await erc20Token.allowance(accounts[1], tokenSwap.address);
     assert(allowance.isZero(),"Not Zero");
     
-    receipt = await token.approve(token.address, value); //accounts[0] has approved accounts[1] for 100 tokens
-    allowance = await token.allowance(accounts[1], token.address); //allowance =100
+    receipt = await erc20Token.approve(tokenSwap.address, value); //accounts[0] has approved accounts[1] for 100 tokens
+    allowance = await erc20Token.allowance(accounts[1], tokenSwap.address); //allowance =100
     console.log(Number(allowance))
     assert((allowance).eq(value), "Not equal to value");
 });   
 
 it("should not swap token1 if not approved", async () => { 
-    await expectRevert( token.swap(), "Token1 allowance too low" ); 
+    await expectRevert( tokenSwap.swap(), "Token1 allowance too low" ); 
 });
 
 
 it("should swap token2 when approved", async () => { let allowance; let receipt; const value = web3.utils.toBN(100);
 
-    allowance = await token.allowance(accounts[3], contract_addr.address);
+    allowance = await erc20Token.allowance(accounts[3], tokenSwap.address);
     assert(allowance.isZero());
     
-    receipt = await token.approve(contract_addr.address, value); //accounts[0] has approved accounts[1] for 100 tokens
-    allowance = await token.allowance(accounts[3], contract_addr.address); //allowance =100
+    receipt = await erc20Token.approve(tokenSwap.address, value); //accounts[0] has approved accounts[1] for 100 tokens
+    allowance = await erc20Token.allowance(accounts[3], tokenSwap.address); //allowance =100
     assert(allowance.eq(value));
 });   
 
 it("should not swap token2 if not approved", async () => { 
-    await expectRevert( token.swap(), "Token2 allowance too low" ); 
+    await expectRevert( tokenSwap.swap(), "Token2 allowance too low" ); 
 });
 
 
 
 it("should tranfer swapped tokens", async () => {
-    await token._safeTransferFrom(accounts[0],accounts[1],accounts[3],amount1);
-    const done=await token.transferFrom(accounts[1],accounts[2],amount1);
+    await tokenSwap._safeTransferFrom(accounts[0],accounts[1],accounts[3],amount1);
+    const done=await erc20Token.transferFrom(accounts[1],accounts[2],amount1);
     assert.isTrue(done);
 });
 

2156cb4299471952813b036ba68c4bf3f5551632
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-04 14:46:54 +0530

    update sol

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -16,7 +16,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 //     event Approval(address indexed tokenOwner,address indexed spender,uint256 tokens);
 // }
 
-contract TokenSwap is ERC20("sk", "sk") {
+contract TokenSwap is ERC20("token", "token") {
     IERC20 public token1;
     address public owner1;
     uint public amount1;
@@ -24,36 +24,10 @@ contract TokenSwap is ERC20("sk", "sk") {
     address public owner2;
     uint public amount2;
 
-    constructor(
-        address _token1,
-        address _owner1,
-        uint _amount1,
-        address _token2,
-        address _owner2,
-        uint _amount2
-    ) {
-        token1 = IERC20(_token1);
-        owner1 = _owner1;
-        amount1 = _amount1;
-        token2 = IERC20(_token2);
-        owner2 = _owner2;
-        amount2 = _amount2;
-    }
+//Create constructor
 
-    function swap() public {
-        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
-        require(
-            token1.allowance(owner1, address(this)) >= amount1,
-            "Token 1 allowance too low"
-        );
-        require(
-            token2.allowance(owner2, address(this)) >= amount2,
-            "Token 2 allowance too low"
-        );
 
-        _safeTransferFrom(token1, owner1, owner2, amount1);
-        _safeTransferFrom(token2, owner2, owner1, amount2);
-    }
+//Create swap function
 
     function _safeTransferFrom(
         IERC20 token,
